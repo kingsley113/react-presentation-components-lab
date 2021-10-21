7de14407aa9b75cf669225bfa90200f08562a720
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-10-21 15:10:09 -0700

    commit tree dab60f4994424918051f882d9be25b87625929f1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import SimpleComponent from './components/SimpleComponent';
-import SimplerComponent from './components/SimplerComponent';
+import SimpleComponent from "./components/SimpleComponent";
+import SimplerComponent from "./components/SimplerComponent";
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent
+      handleClick={() => {
+        alert("This is a no touching zone! How dare you!");
+      }}
+    />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
